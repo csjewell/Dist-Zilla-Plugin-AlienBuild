661d595f694d2a170594dc7a4b4680ae619eb7bc
Graham Ollis <plicease@cpan.org>
2017-01-30 15:00:47 -0500

    initial implementation

--- lib/Dist/Zilla/Plugin/AlienBuild.pm
+++ lib/Dist/Zilla/Plugin/AlienBuild.pm
@@ -4,24 +4,83 @@ use strict;
 use warnings;
 use Moose;
 use List::Util qw( first );
+use Path::Tiny qw( path );
 
 # ABSTRACT: Use Alien::Build with Dist::Zilla
 # VERSION
 
 with 'Dist::Zilla::Role::FileMunger';
 with 'Dist::Zilla::Role::MetaProvider';
+with 'Dist::Zilla::Role::PrereqSource';
+
+sub register_prereqs
+{
+  my($self) = @_;
+
+  if(my $file = first { $_->name eq 'alienfile' } @{ $self->zilla->files })
+  {
+    require Alien::Build;
+    my $alienfile = Path::Tiny->tempfile;
+    $alienfile->spew($file->content);
+    my $build = Alien::Build->load($alienfile);
+
+    # Configure requires...
+    $self->zilla->register_prereqs(
+      { phase => 'configure' },
+      'Alien::Build::MM' => '0.01',
+      'ExtUtils::MakeMaker' => '6.52',
+      %{ $build->requires('configure') },
+    );
+    
+    # Build requires...
+    $self->zilla->register_prereqs(
+      { phase => 'build' },
+      'Alien::Build::MM' => '0.01',
+      %{ $build->requires('any') },
+    );
+  }
+  else
+  {
+    $self->log_fatal('No alienfile!');
+  }
+  
+}
+
+my $mm_code_prereqs = <<'EOF1';
+use Alien::Build::MM;
+my $abmm = Alien::Build::MM->new;
+%WriteMakefileArgs = $abmm->mm_args(%WriteMakefileArgs);
+EOF1
+
+my $mm_code_postamble = <<'EOF2';
+sub MY::postamble {
+  $abmm->mm_postamble;
+}
+EOF2
+
+my $comment_begin  = "# BEGIN code inserted by Dist::Zilla::Plugin::AlienBuild\n";
+my $comment_end    = "# END code inserted by Dist::Zilla::Plugin::AlienBuild\n";
 
 sub munge_files
 {
   my($self) = @_;
-  
+
   if(my $file = first { $_->name eq 'Makefile.PL' } @{ $self->zilla->files })
   {
-    die 'todo';
+    my $content = $file->content;
+ 
+    my $ok = $content =~ s/(unless \( eval \{ ExtUtils::MakeMaker)/"$comment_begin$mm_code_prereqs$comment_end\n\n$1"/e;
+    $self->log_fatal('unable to find the correct location to insert prereqs')
+      unless $ok;
+    
+    $content .= "\n\n$comment_begin$mm_code_postamble$comment_end\n";
+    
+    $file->content($content);
   }
   
   elsif($file = first { $_->name eq 'Makefile.PL' } @{ $self->zilla->files })
   {
+    # TODO: support MB
     $self->log_fatal('Build.PL not (yet) supported');
   }
   
@@ -33,7 +92,7 @@ sub munge_files
 
 sub metadata {
   my($self) = @_;
-  { dynamic_config => $self->dynamic_config };
+  { dynamic_config => 1 };
 }
 
 
--- t/dist_zilla_plugin_alienbuild.t
+++ t/dist_zilla_plugin_alienbuild.t
@@ -1,8 +1,68 @@
 use Test2::Bundle::Extended;
+use Test::DZil;
 use Dist::Zilla::Plugin::AlienBuild;
+use JSON::PP qw( decode_json );
+use List::Util qw( first );
 
-ok 1;
+{ package Foo::Config; our $VERSION = '1.00'; $INC{'Foo/Config.pm'} = __FILE__ }
 
-diag 'TODO';
+subtest 'basic' => sub {
+
+  my $tzil = Builder->from_config({ dist_root => 'corpus/Alien-Foo1' }, {
+    add_files => {
+      'source/dist.ini' => simple_ini(
+        { name => 'Alien-Foo1' },
+        [ 'GatherDir'  => {} ],
+        [ 'MakeMaker'  => {} ],
+        [ 'MetaJSON'   => {} ],
+        [ 'AlienBuild' => {} ],
+      ),
+    },
+  });
+  
+  $tzil->build;
+
+  foreach my $file (@{ $tzil->files })
+  {
+    note "[@{[ $file->name ]}]";
+    note $file->content;
+  }
+
+  my $meta = decode_json((first { $_->name eq 'META.json' } @{ $tzil->files })->content);
+  
+  is(
+    $meta->{dynamic_config},
+    T(),
+    'dynamic config is set in META.json',
+  );
+  
+  is(
+    $meta->{prereqs}->{configure}->{requires},
+    hash {
+      field 'Alien::Build::MM'    => T();
+      field 'Foo::Config'         => '0.55';
+      field 'ExtUtils::MakeMaker' => E();
+      etc;
+    },
+    'configure prereqs',
+  );
+
+  is(
+    $meta->{prereqs}->{build}->{requires},
+    hash {
+      field 'Alien::Build::MM' => T();
+      field 'Foo::Build' => '0.01';
+      etc;
+    },
+    'build prereqs',
+  );
+  
+  my $makefile_pl = (first { $_->name eq 'Makefile.PL' } @{ $tzil->files })->content;
+  
+  like $makefile_pl, qr{Alien::Build::MM}, 'reference to AB::MM';
+
+};
 
 done_testing;
+
+
